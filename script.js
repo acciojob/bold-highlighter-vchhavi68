5b7e21577c3031a01035813bbc3246ac814c1d90
vchhavi68 <vchhavi68@gmail.com>
2026-01-06 23:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function highlight() {
     //Write your code here
-const strongElements = document.querySelectorAll('strong');
+	const strongElements = document.querySelectorAll('strong');
+
     strongElements.forEach(elem => {
         elem.style.color = 'rgb(0, 128, 0)';
     });
-}
 
+
+}
 
 
 function return_normal() {
     //Write your code here
 	const strongElements = document.querySelectorAll('strong');
+    
 
     strongElements.forEach(elem => {
         elem.style.color = 'rgb(0, 0, 0)';
     });
+
+    
 }
